7379cc7d611992f6ff41c39ce7340e86df5769bd
Octávio Turra <octavio.turra@gmail.com>
2017-05-19 00:40:48 -0300

    console get facebok user data

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ const actions = {
   sendPoints({sessionId, context : ctx, entities}) {
 	  const {number, context, contact, reason} = entities;
 	return getFacebookUserData(ctx.id)
-	.then(data => sendPoints(data.first_name, contact, number, context, reason))
+	.then(data => console.log(data, data get facebook user data'') || sendPoints(data.first_name, contact, number, context, reason))
 	.then(spreadsheet => new Promise((resolve, reject) => response.updates.updatedRows ? resolve(ontext) : reject({ err: 'no update' })));
   }
 };
