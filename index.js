ede1ccde0365da5239d9b917bc2d7213d10a2239
Octávio Turra <octavio.turra@gmail.com>
2017-05-27 23:48:09 -0300

    removing webhook

--- index.js
+++ index.js
@@ -157,17 +157,7 @@ function authorize() {
 
 // ----------------------------------------------------------------------------
 // Telegram
-const options = {
-  webHook: {
-    // Port to which you should bind is assigned to $PORT variable
-    // See: https://devcenter.heroku.com/articles/dynos#local-environment-variables
-    port: process.env.PORT
-    // you do NOT need to set up certificates since Heroku provides
-    // the SSL certs already (https://<app-name>.herokuapp.com)
-    // Also no need to pass IP because on Heroku you need to bind to 0.0.0.0
-  }
-};
-const bot = new TelegramBot(TELEGRAM_TOKEN, options);
+const bot = new TelegramBot(TELEGRAM_TOKEN);
 // Heroku routes from port :443 to $PORT
 // Add URL of your app to env variable or enable Dyno Metadata
 // to get this automatically
